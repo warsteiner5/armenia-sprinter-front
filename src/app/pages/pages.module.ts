1c827b8ca43d75595e0baecc9acb01fdad986568
Ковальчук Алексей <kovalchuk.aleksey@outlook.com>
2022-01-26 21:36:34 +0300

    Init commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {TransferComponent} from "./transfer/transfer.component";
+
+
+
+@NgModule({
+  declarations: [
+    TransferComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
